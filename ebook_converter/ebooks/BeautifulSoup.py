eb34f87ea44e570189403115dacf179e1d5f1295
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2022-04-28 01:37:20 +0800

    Make html5_parser optional because I can't install it.

--- ebook_converter/ebooks/BeautifulSoup.py
+++ ebook_converter/ebooks/BeautifulSoup.py
@@ -1,12 +1,19 @@
 # License: GPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
+is_html5_parser_exists = True
 import bs4
-from html5_parser import soup as html5_soup
+try:
+    from html5_parser import soup as html5_soup
+except:
+    is_html5_parser_exists = False
 
 from ebook_converter.ebooks import chardet
 from ebook_converter.utils import cleantext
 
 
 def parse_html(markup):
+    if not is_html5_parser_exists:
+        raise RuntimeError("No html5_parser")
+
     if isinstance(markup, str):
         markup = chardet.strip_encoding_declarations(markup)
         markup = chardet.substitute_entites(markup)
