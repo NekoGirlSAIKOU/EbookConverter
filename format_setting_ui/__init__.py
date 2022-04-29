6635e09dc4592d78f43fdf6bd28000a98f88db57
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2022-04-30 00:29:41 +0800

    UI changes

--- format_setting_ui/__init__.py
+++ format_setting_ui/__init__.py
@@ -2,11 +2,15 @@ from typing import Dict, Optional, Union
 
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.list import MDList
 from kivymd.uix.textfield import MDTextField
+Builder.load_file('format_setting_ui/BaseSettingUi.kv')
 
 Builder.load_file('format_setting_ui/BaseOutputSettingUi.kv')
 Builder.load_file('format_setting_ui/MobiOutputSettingUi.kv')
+
+Builder.load_file('format_setting_ui/BaseInputSettingUi.kv')
 
 
 class BaseSettingUi(MDList):
@@ -72,3 +76,26 @@ class MobiOutputSettingUi(BaseOutputSettingUi):
     def update_setting(self):
         super(MobiOutputSettingUi, self).update_setting()
         self.setting_map['--mobi-file-type'] = self.tf_file_type.text
+
+
+class BaseInputSettingUi(BaseSettingUi):
+    tf_book_title: MDTextField = ObjectProperty()
+
+    def __init__(self, setting_map: Dict[str, Optional[Union[str, bool]]], **kwargs):
+        super().__init__(setting_map, **kwargs)
+        self.support_settings.extend([
+            '--title'
+        ])
+
+    def fill_settings(self):
+        """FIll settings map using default settings"""
+        self.setting_map['--title'] = self.setting_map.get('--title', '')
+
+    def update_ui(self):
+        """Update UI to show updated setting"""
+        self.tf_book_title.text = self.setting_map['--title']
+
+    def update_setting(self):
+        """update setting to save UI changes"""
+        print('update_setting: save tf_book_title: ', self.tf_book_title.text)
+        self.setting_map['--title'] = self.tf_book_title.text
