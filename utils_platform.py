e0b87d806a81952a6e7e01a6ef1675cf3dbbb8e2
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2022-06-03 00:05:29 +0800

    Only use SAF implementation when there is no storage permission

--- utils_platform.py
+++ utils_platform.py
@@ -1,3 +1,4 @@
+import os
 from typing import Optional
 
 from plyer.facades import FileChooser
@@ -8,8 +9,15 @@ def get_file_chooser() -> Optional[FileChooser]:
     if platform == 'android':
         # from plyer.platforms.android.filechooser import AndroidFileChooser
         from android_file_chooser_saf import AndroidFileChooserSAF
-        return AndroidFileChooserSAF()
-
+        from my_android_file_chooser import MyAndroidFileChooser
+        try:
+            os.listdir('/sdcard/Android')
+        except:
+            return AndroidFileChooserSAF()
+        else:
+            # If we have the permission to read and write to external storage, there
+            # is no need to use the SAF implementation.
+            return MyAndroidFileChooser()
     elif platform == 'ios':
         from plyer.platforms.ios.filechooser import IOSFileChooser
         return IOSFileChooser()
